a7e9fc457bbf1ce633981e1999514e7aa6d70f41
Max <MaxMB15@gmail.com>
2017-08-12 15:23:27 -0700

    added getUrlSourceCode method to scrape html pages

--- JavaPremAnalysts/src/getPremData.java
+++ JavaPremAnalysts/src/getPremData.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class getPremData {
-	static String[] getLatestData(){
-		//need to format data to decode 
-	}
+	/*static String[] getLatestData(){
+		//need to format data to decode
+		
+	}*/
+	static String getUrlSourceCode(String url) throws IOException {
+		//Make a new URL object and make a connection
+        URL WebPage = new URL(url);
+        URLConnection connection = WebPage.openConnection();
+        
+        //Read InputStream
+        BufferedReader data = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        //Temp string holder
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = data.readLine()) != null){
+        	a.append(inputLine);
+        }
+        data.close();
+        return a.toString();
+    }
 }
