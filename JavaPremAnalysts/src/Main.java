ef717a0a6a4ea93c44e765ddb265c429839e3fd9
Max <MaxMB15@gmail.com>
2017-08-14 23:48:32 -0700

    fixed a small main method bug that would print out all sourceCode arrays incorrectly

--- JavaPremAnalysts/src/Main.java
+++ JavaPremAnalysts/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 	public static void main(String[] args){
 		SourceCode sc = new SourceCode(SourceCode.getURL("https://www.premierleague.com/players?se=79&cl=43"));
 		System.out.println(sc.toString());
-		System.out.println(sc.getNodesByName("tr").toString());
+		for(SourceCode sca : sc.getNodesByName("tr")){
+			System.out.println(sca.toString());			
+		}
 	}
 }
